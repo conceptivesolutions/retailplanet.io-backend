e8897f5493925130159816089d4caee02f4ee462
w.glanzer <wernerglanzer@googlemail.com>
2019-06-20 01:01:27 +0200

    Replaced the old "IDynamicConfig" with new annotationprocessor to generate application.properties on-the-fly

--- services/businessapi/src/main/java/io/retailplanet/backend/businessapi/impl/IEvents.java
+++ services/businessapi/src/main/java/io/retailplanet/backend/businessapi/impl/IEvents.java
@@ -1,51 +1,34 @@
 package io.retailplanet.backend.businessapi.impl;
 
-import io.retailplanet.backend.common.api.IDynamicConfig;
+import io.retailplanet.backend.common.api.comm.*;
 import io.vertx.kafka.client.serialization.*;
 import org.apache.kafka.common.serialization.*;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * This interface contains all events, incoming and outgoing
  *
  * @author w.glanzer, 11.06.2019
  */
+@EventContainer(groupID = "businessapi")
 public interface IEvents
 {
 
   /**
    * Event: BusinessToken should be created
    */
+  @OutgoingEvent(keySerializer = StringSerializer.class, valueSerializer = JsonObjectSerializer.class)
   String OUT_BUSINESSTOKEN_CREATE = "BusinessToken_CREATE_OUT";
 
   /**
    * Event: Indicates, that a BusinessToken was created
    */
+  @IncomingEvent(autoOffsetReset = "latest", keyDeserializer = StringDeserializer.class, valueDeserializer = JsonObjectDeserializer.class)
   String IN_BUSINESSTOKEN_CREATED = "BusinessToken_CREATED_IN";
 
   /**
    * Event: Indicates, that a BusinessToken could not be created
    */
+  @IncomingEvent(autoOffsetReset = "latest", keyDeserializer = StringDeserializer.class, valueDeserializer = JsonObjectDeserializer.class)
   String IN_BUSINESSTOKEN_CREATE_FAILED = "BusinessToken_CREATE_FAILED_IN";
-
-  /**
-   * Configuration for Service: businessapi
-   */
-  @IDynamicConfig.Provider
-  class DynamicConfProvider implements IDynamicConfig
-  {
-    private static final String _GROUP_ID = "businessapi";
-
-    @NotNull
-    @Override
-    public Builder generate()
-    {
-      return Builder.create()
-          .defaultValue("bootstrap.servers", "${KAFKA_SERVERS}")
-          .topic(TopicBuilder.createRead(IN_BUSINESSTOKEN_CREATED, _GROUP_ID, "latest", StringDeserializer.class, JsonObjectDeserializer.class))
-          .topic(TopicBuilder.createRead(IN_BUSINESSTOKEN_CREATE_FAILED, _GROUP_ID, "latest", StringDeserializer.class, JsonObjectDeserializer.class))
-          .topic(TopicBuilder.createWrite(OUT_BUSINESSTOKEN_CREATE, StringSerializer.class, JsonObjectSerializer.class));
-    }
-  }
 
 }
