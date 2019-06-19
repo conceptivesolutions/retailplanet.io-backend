d44dbf452efbfa5a56e9284014f0dffa7583237f
w.glanzer <wernerglanzer@googlemail.com>
2019-06-20 01:01:27 +0200

    Replaced the old "IDynamicConfig" with new annotationprocessor to generate application.properties on-the-fly

--- services/userauth/src/main/java/io/retailplanet/backend/userauth/impl/IEvents.java
+++ services/userauth/src/main/java/io/retailplanet/backend/userauth/impl/IEvents.java
@@ -1,48 +1,28 @@
 package io.retailplanet.backend.userauth.impl;
 
-import io.retailplanet.backend.common.api.IDynamicConfig;
+import io.retailplanet.backend.common.api.comm.*;
 import io.vertx.kafka.client.serialization.*;
 import org.apache.kafka.common.serialization.*;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * This interface contains all events, incoming and outgoing
  *
  * @author w.glanzer, 11.06.2019
  */
+@EventContainer(groupID = "userauth")
 public interface IEvents
 {
 
   /**
    * Event: BusinessToken should be created
    */
+  @IncomingEvent(autoOffsetReset = "latest", keyDeserializer = StringDeserializer.class, valueDeserializer = JsonObjectDeserializer.class)
   String IN_BUSINESSTOKEN_CREATE = "BusinessToken_CREATE_IN";
 
   /**
    * Event: BusinessToken should be created, client was authorized
    */
+  @OutgoingEvent(keySerializer = StringSerializer.class, valueSerializer = JsonObjectSerializer.class)
   String OUT_BUSINESSTOKEN_CREATE_AUTH = "BusinessToken_CREATE_AUTH_OUT";
 
-  /**
-   * Configuration for Service: userauth
-   */
-  @IDynamicConfig.Provider
-  class DynamicConfProvider implements IDynamicConfig
-  {
-    private static final String _GROUP_ID = "userauth";
-
-    public DynamicConfProvider()
-    {
-    }
-
-    @NotNull
-    @Override
-    public Builder generate()
-    {
-      return Builder.create()
-          .defaultValue("bootstrap.servers", "${KAFKA_SERVERS}")
-          .topic(TopicBuilder.createRead(IN_BUSINESSTOKEN_CREATE, _GROUP_ID, "latest", StringDeserializer.class, JsonObjectDeserializer.class))
-          .topic(TopicBuilder.createWrite(OUT_BUSINESSTOKEN_CREATE_AUTH, StringSerializer.class, JsonObjectSerializer.class));
-    }
-  }
 }
