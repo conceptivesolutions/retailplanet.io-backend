e44fa7fea2fee2e93c44c238e2d35a7ab0d24569
w.glanzer <wernerglanzer@googlemail.com>
2019-06-20 21:29:43 +0200

    Initial commit

---
+++ services/products/src/main/java/io/retailplanet/backend/businessapi/impl/IEvents.java
@@ -0,0 +1,13 @@
+package io.retailplanet.backend.businessapi.impl;
+
+/**
+ * This interface contains all events, incoming and outgoing
+ *
+ * @author w.glanzer, 20.06.2019
+ */
+public interface IEvents
+{
+
+
+
+}
