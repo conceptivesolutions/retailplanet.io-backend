70f9c7e8762b478f8b1685761dad5ae7146518c8
w.glanzer <wernerglanzer@googlemail.com>
2019-06-20 09:43:32 +0200

    Refactored Incoming- and OutgoingEvent to use our own EventSerializers and -Deserializers

--- services/businesstoken/src/main/java/io/retailplanet/backend/businesstoken/impl/events/IEvents.java
+++ services/businesstoken/src/main/java/io/retailplanet/backend/businesstoken/impl/events/IEvents.java
@@ -1,8 +1,6 @@
 package io.retailplanet.backend.businesstoken.impl.events;
 
 import io.retailplanet.backend.common.api.comm.*;
-import io.vertx.kafka.client.serialization.JsonObjectDeserializer;
-import org.apache.kafka.common.serialization.*;
 
 /**
  * This interface contains all events, incoming and outgoing
@@ -16,19 +14,19 @@ public interface IEvents
   /**
    * Event: a new BusinessToken should be created
    */
-  @IncomingEvent(autoOffsetReset = "latest", keyDeserializer = StringDeserializer.class, valueDeserializer = JsonObjectDeserializer.class)
+  @IncomingEvent
   String IN_BUSINESSTOKEN_CREATE_AUTH = "BusinessToken_CREATE_AUTH_IN";
 
   /**
    * Event: a new BusinessToken was created
    */
-  @IncomingEvent(autoOffsetReset = "earliest", keyDeserializer = StringDeserializer.class, valueDeserializer = JsonObjectDeserializer.class)
+  @IncomingEvent(autoOffsetReset = "earliest")
   String IN_BUSINESSTOKEN_CREATED = "BusinessToken_CREATED_IN";
 
   /**
    * Event: a new BusinessToken was created
    */
-  @OutgoingEvent(keySerializer = StringSerializer.class, valueSerializer = JsonObjectDeserializer.class)
+  @OutgoingEvent
   String OUT_BUSINESSTOKEN_CREATED = "BusinessToken_CREATED_OUT";
 
 }
