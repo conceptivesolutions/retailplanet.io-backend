04043b8ad43b68ee3152296cbcaeb2d02b6ffa43
w.glanzer <wernerglanzer@googlemail.com>
2019-06-20 09:43:32 +0200

    Refactored Incoming- and OutgoingEvent to use our own EventSerializers and -Deserializers

--- services/businessapi/src/main/java/io/retailplanet/backend/businessapi/impl/IEvents.java
+++ services/businessapi/src/main/java/io/retailplanet/backend/businessapi/impl/IEvents.java
@@ -1,8 +1,6 @@
 package io.retailplanet.backend.businessapi.impl;
 
 import io.retailplanet.backend.common.api.comm.*;
-import io.vertx.kafka.client.serialization.*;
-import org.apache.kafka.common.serialization.*;
 
 /**
  * This interface contains all events, incoming and outgoing
@@ -16,19 +14,19 @@ public interface IEvents
   /**
    * Event: BusinessToken should be created
    */
-  @OutgoingEvent(keySerializer = StringSerializer.class, valueSerializer = JsonObjectSerializer.class)
+  @OutgoingEvent
   String OUT_BUSINESSTOKEN_CREATE = "BusinessToken_CREATE_OUT";
 
   /**
    * Event: Indicates, that a BusinessToken was created
    */
-  @IncomingEvent(autoOffsetReset = "latest", keyDeserializer = StringDeserializer.class, valueDeserializer = JsonObjectDeserializer.class)
+  @IncomingEvent
   String IN_BUSINESSTOKEN_CREATED = "BusinessToken_CREATED_IN";
 
   /**
    * Event: Indicates, that a BusinessToken could not be created
    */
-  @IncomingEvent(autoOffsetReset = "latest", keyDeserializer = StringDeserializer.class, valueDeserializer = JsonObjectDeserializer.class)
+  @IncomingEvent
   String IN_BUSINESSTOKEN_CREATE_FAILED = "BusinessToken_CREATE_FAILED_IN";
 
 }
