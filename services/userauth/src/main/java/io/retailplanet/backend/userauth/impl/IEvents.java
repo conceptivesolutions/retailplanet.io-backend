d940e3b949ce4574105fbeab3a01636fab73afe8
w.glanzer <wernerglanzer@googlemail.com>
2019-06-20 09:43:32 +0200

    Refactored Incoming- and OutgoingEvent to use our own EventSerializers and -Deserializers

--- services/userauth/src/main/java/io/retailplanet/backend/userauth/impl/IEvents.java
+++ services/userauth/src/main/java/io/retailplanet/backend/userauth/impl/IEvents.java
@@ -1,8 +1,6 @@
 package io.retailplanet.backend.userauth.impl;
 
 import io.retailplanet.backend.common.api.comm.*;
-import io.vertx.kafka.client.serialization.*;
-import org.apache.kafka.common.serialization.*;
 
 /**
  * This interface contains all events, incoming and outgoing
@@ -16,13 +14,13 @@ public interface IEvents
   /**
    * Event: BusinessToken should be created
    */
-  @IncomingEvent(autoOffsetReset = "latest", keyDeserializer = StringDeserializer.class, valueDeserializer = JsonObjectDeserializer.class)
+  @IncomingEvent
   String IN_BUSINESSTOKEN_CREATE = "BusinessToken_CREATE_IN";
 
   /**
    * Event: BusinessToken should be created, client was authorized
    */
-  @OutgoingEvent(keySerializer = StringSerializer.class, valueSerializer = JsonObjectSerializer.class)
+  @OutgoingEvent
   String OUT_BUSINESSTOKEN_CREATE_AUTH = "BusinessToken_CREATE_AUTH_OUT";
 
 }
