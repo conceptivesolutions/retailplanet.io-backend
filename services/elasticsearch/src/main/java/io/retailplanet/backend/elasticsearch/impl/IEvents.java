a25a2d3ce63283a2c1bfaebc4410066049476f1d
w.glanzer <wernerglanzer@googlemail.com>
2019-06-22 15:34:36 +0200

    Initial commit

---
+++ services/elasticsearch/src/main/java/io/retailplanet/backend/elasticsearch/impl/IEvents.java
@@ -0,0 +1,13 @@
+package io.retailplanet.backend.elasticsearch.impl;
+
+/**
+ * This interface contains all events, incoming and outgoing
+ *
+ * @author w.glanzer, 22.06.2019
+ */
+public interface IEvents
+{
+
+
+
+}
