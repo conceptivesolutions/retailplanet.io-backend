cd7515989f6770aa2f0c4f5388bf8544d2d54fc5
w.glanzer <wernerglanzer@googlemail.com>
2019-06-23 10:37:34 +0200

    Added "docker-update.sh" to update all docker images at once, included elasticsearch, markets and products in docker-compose.yml

---
+++ docker-update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker pull images.conceptive.io/retailplanet.backend.businessapi:latest
+docker pull images.conceptive.io/retailplanet.backend.businesstoken:latest
+docker pull images.conceptive.io/retailplanet.backend.elasticsearch:latest
+docker pull images.conceptive.io/retailplanet.backend.markets:latest
+docker pull images.conceptive.io/retailplanet.backend.products:latest
+docker pull images.conceptive.io/retailplanet.backend.userauth:latest
