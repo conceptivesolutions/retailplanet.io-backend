315eed182d6a334350a55650be2d1ce70803ae69
w.glanzer <wernerglanzer@googlemail.com>
2019-06-25 20:41:15 +0200

    Changed docker images from "normal" java images to native images

--- docker-update.sh
+++ docker-update.sh
@@ -6,3 +6,11 @@ docker pull images.conceptive.io/retailplanet.backend.elasticsearch:latest
 docker pull images.conceptive.io/retailplanet.backend.markets:latest
 docker pull images.conceptive.io/retailplanet.backend.products:latest
 docker pull images.conceptive.io/retailplanet.backend.userauth:latest
+
+# Native
+docker pull images.conceptive.io/retailplanet.backend.businessapi.native:latest
+#docker pull images.conceptive.io/retailplanet.backend.businesstoken.native:latest
+docker pull images.conceptive.io/retailplanet.backend.elasticsearch.native:latest
+docker pull images.conceptive.io/retailplanet.backend.markets.native:latest
+docker pull images.conceptive.io/retailplanet.backend.products.native:latest
+docker pull images.conceptive.io/retailplanet.backend.userauth.native:latest
