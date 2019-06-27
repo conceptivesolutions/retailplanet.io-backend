866a856fa61e9baaa7f198b3310f92908a463395
w.glanzer <wernerglanzer@googlemail.com>
2019-06-27 22:17:03 +0200

    Try serializing ProductAvailability and Markets with "JsonbProperty"

--- services/products/src/main/java/io/retailplanet/backend/products/impl/struct/ProductAvailability.java
+++ services/products/src/main/java/io/retailplanet/backend/products/impl/struct/ProductAvailability.java
@@ -1,13 +1,15 @@
 package io.retailplanet.backend.products.impl.struct;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.vertx.core.json.JsonObject;
 import org.jetbrains.annotations.NotNull;
 
-import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.annotation.*;
 
 /**
  * @author w.glanzer, 21.06.2019
  */
+@RegisterForReflection
 public class ProductAvailability
 {
 
@@ -16,6 +18,11 @@ public class ProductAvailability
 
   @JsonbProperty
   public int quantity;
+
+  @JsonbCreator
+  public ProductAvailability()
+  {
+  }
 
   @SuppressWarnings("unused") // JSON
   public enum TYPE
