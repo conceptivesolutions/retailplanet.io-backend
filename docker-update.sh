fc9a9a92bc28c6f20dd84bfce8b150dfbf032375
w.glanzer <wernerglanzer@googlemail.com>
2019-07-10 13:42:37 +0200

    Do not pull all non-native images, only that ones which are needed

--- docker-update.sh
+++ docker-update.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-docker pull images.conceptive.io/retailplanet.backend.businessapi:latest
+#docker pull images.conceptive.io/retailplanet.backend.businessapi:latest
 docker pull images.conceptive.io/retailplanet.backend.businesstoken:latest
-docker pull images.conceptive.io/retailplanet.backend.elasticsearch:latest
-docker pull images.conceptive.io/retailplanet.backend.markets:latest
-docker pull images.conceptive.io/retailplanet.backend.products:latest
-docker pull images.conceptive.io/retailplanet.backend.userauth:latest
+#docker pull images.conceptive.io/retailplanet.backend.elasticsearch:latest
+#docker pull images.conceptive.io/retailplanet.backend.markets:latest
+#docker pull images.conceptive.io/retailplanet.backend.products:latest
+#docker pull images.conceptive.io/retailplanet.backend.userauth:latest
 
 # Native
 docker pull images.conceptive.io/retailplanet.backend.businessapi.native:latest
