f215e880f1b5445440abd2a9f3d5a3576e9209c6
w.glanzer <wernerglanzer@googlemail.com>
2019-07-13 13:04:35 +0200

    Implemented simple search in "products" service

---
+++ services/products/src/main/java/io/retailplanet/backend/products/api/SearchService.java
@@ -0,0 +1,48 @@
+package io.retailplanet.backend.products.api;
+
+import io.retailplanet.backend.common.events.search.*;
+import io.retailplanet.backend.products.impl.IEvents;
+import io.retailplanet.backend.products.impl.index.IIndexFacade;
+import io.smallrye.reactive.messaging.annotations.*;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.jetbrains.annotations.Nullable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+
+/**
+ * Service: Product search
+ *
+ * @author w.glanzer, 12.07.2019
+ */
+@ApplicationScoped
+public class SearchService
+{
+
+  @Inject
+  private IIndexFacade indexFacade;
+
+  @Stream(IEvents.OUT_SEARCH_PRODUCTS_RESULT)
+  Emitter<SearchProductsResultEvent> resultEmitter;
+
+  /**
+   * Executes product search
+   *
+   * @param pEvent Search event
+   */
+  @Incoming(IEvents.IN_SEARCH_PRODUCTS)
+  public void searchProducts(@Nullable SearchProductsEvent pEvent)
+  {
+    if (pEvent == null)
+      return;
+
+    // search in index
+    indexFacade.searchProducts(pEvent.query, pEvent.sorting, pEvent.offset, pEvent.length, pEvent.filter)
+        .subscribe(pResult -> resultEmitter.send(pEvent.createAnswer(SearchProductsResultEvent.class)
+                                                     .maxSize(pResult.maxSize)
+                                                     .filters(pResult.filters)
+                                                     .elements(new ArrayList<>(pResult.elements))));
+  }
+
+}
--- services/products/src/main/java/io/retailplanet/backend/products/impl/index/StreamedIndexFacade.java
+++ services/products/src/main/java/io/retailplanet/backend/products/impl/index/StreamedIndexFacade.java
@@ -1,14 +1,16 @@
 package io.retailplanet.backend.products.impl.index;
 
-import io.retailplanet.backend.common.events.index.DocumentUpsertEvent;
+import io.reactivex.*;
+import io.retailplanet.backend.common.events.index.*;
 import io.retailplanet.backend.products.impl.IEvents;
-import io.retailplanet.backend.products.impl.struct.Product;
+import io.retailplanet.backend.products.impl.struct.*;
+import io.smallrye.reactive.messaging.annotations.Emitter;
 import io.smallrye.reactive.messaging.annotations.*;
 import io.vertx.core.json.JsonArray;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.*;
 
 import javax.inject.Singleton;
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Collector;
 
 /**
@@ -25,6 +27,12 @@ class StreamedIndexFacade implements IIndexFacade
   @Stream(IEvents.OUT_INDEX_DOCUMENT_UPSERT)
   Emitter<DocumentUpsertEvent> upsertProductsInIndex;
 
+  @Stream(IEvents.OUT_INDEX_DOCUMENT_SEARCH)
+  Emitter<DocumentSearchEvent> searchInIndex;
+
+  @Stream(IEvents.IN_INDEX_DOCUMENT_SEARCHRESULT)
+  Flowable<DocumentSearchResultEvent> searchResultsFlowable;
+
   @Override
   public void upsertProducts(@NotNull String pClientID, @NotNull Product[] pProductList)
   {
@@ -33,11 +41,31 @@ class StreamedIndexFacade implements IIndexFacade
         .clientID(pClientID)
         .type(_INDEX_TYPE)
         .doc(Arrays.stream(pProductList)
-            .map(pProduct -> pProduct.toIndexJSON(pClientID))
-            .collect(Collector.of(JsonArray::new, JsonArray::add, JsonArray::addAll)));
+                 .map(pProduct -> pProduct.toIndexJSON(pClientID))
+                 .collect(Collector.of(JsonArray::new, JsonArray::add, JsonArray::addAll)));
 
     // fire request
     upsertProductsInIndex.send(event);
+  }
+
+  @NotNull
+  @Override
+  public Single<SearchResult> searchProducts(@NotNull String pQuery, @Nullable String pSort, @Nullable Integer pOffset, @Nullable Integer pLength,
+                                             @Nullable Map<String, Object> pFilters)
+  {
+    // Build request
+    DocumentSearchEvent event = new DocumentSearchEvent()
+        .query(new DocumentSearchEvent.Query()
+                   .matches(IIndexStructure.IProduct.NAME, pQuery))
+        .offset(pOffset)
+        .length(pLength);
+
+    // send
+    searchInIndex.send(event);
+
+    // wait for answer
+    return event.waitForAnswer(searchResultsFlowable)
+        .map(pEvent -> new SearchResult()); //todo transform
   }
 
 
