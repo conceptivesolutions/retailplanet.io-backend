f66ac04969bd7d86e22aa97f18288423e7525b4a
w.glanzer <wernerglanzer@googlemail.com>
2019-07-20 22:57:41 +0200

    Added missing "retailplanet.backend.search.native" image to docker-compose and docker-update

--- docker-update.sh
+++ docker-update.sh
@@ -3,6 +3,7 @@
 #docker pull images.conceptive.io/retailplanet.backend.businessapi:latest
 docker pull images.conceptive.io/retailplanet.backend.businesstoken:latest
 #docker pull images.conceptive.io/retailplanet.backend.elasticsearch:latest
+#docker pull images.conceptive.io/retailplanet.backend.search:latest
 #docker pull images.conceptive.io/retailplanet.backend.markets:latest
 #docker pull images.conceptive.io/retailplanet.backend.products:latest
 #docker pull images.conceptive.io/retailplanet.backend.userauth:latest
@@ -11,6 +12,7 @@ docker pull images.conceptive.io/retailplanet.backend.businesstoken:latest
 docker pull images.conceptive.io/retailplanet.backend.businessapi.native:latest
 #docker pull images.conceptive.io/retailplanet.backend.businesstoken.native:latest
 docker pull images.conceptive.io/retailplanet.backend.elasticsearch.native:latest
+docker pull images.conceptive.io/retailplanet.backend.search.native:latest
 docker pull images.conceptive.io/retailplanet.backend.markets.native:latest
 docker pull images.conceptive.io/retailplanet.backend.products.native:latest
 docker pull images.conceptive.io/retailplanet.backend.userauth.native:latest
