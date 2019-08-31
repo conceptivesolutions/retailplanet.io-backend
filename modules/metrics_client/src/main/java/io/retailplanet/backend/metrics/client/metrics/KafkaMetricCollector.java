c000cce0638d74264632004cbd0296abe28abc86
w.glanzer <wernerglanzer@googlemail.com>
2019-08-31 13:59:49 +0200

    KafkaMetricCollector: Distribute "roundtripTime" with a single gauge

--- modules/metrics_client/src/main/java/io/retailplanet/backend/metrics/client/metrics/KafkaMetricCollector.java
+++ modules/metrics_client/src/main/java/io/retailplanet/backend/metrics/client/metrics/KafkaMetricCollector.java
@@ -3,13 +3,13 @@ package io.retailplanet.backend.metrics.client.metrics;
 import io.retailplanet.backend.common.events.metric.KafkaMetricEvent;
 import io.retailplanet.backend.metrics.client.MetricEventFacade;
 import org.eclipse.microprofile.metrics.MetricUnits;
-import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
 import org.slf4j.*;
 
 import javax.enterprise.context.*;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import java.time.*;
+import java.time.Instant;
 
 /**
  * Collectors metrics for interaction with kafka
@@ -17,54 +17,31 @@ import java.time.*;
  * @author w.glanzer, 29.08.2019
  */
 @ApplicationScoped
-public class KafkaMetricCollector implements Runnable
+public class KafkaMetricCollector
 {
 
-  private static final Duration _INTERVAL = Duration.ofSeconds(15);
   private static final Logger _LOGGER = LoggerFactory.getLogger(KafkaMetricCollector.class);
 
   @Inject
   private MetricEventFacade eventFacade;
 
-  private Thread collectorThread;
-
-  public void init(@SuppressWarnings("unused") @Observes @Initialized(ApplicationScoped.class) Object pInit)
+  @SuppressWarnings("unused")
+  public void init(@Observes @Initialized(ApplicationScoped.class) Object pInit)
   {
-    if (collectorThread != null)
-      return;
-    collectorThread = new Thread(this, "tRoundtripMetricCollector");
-    collectorThread.setDaemon(true);
-    collectorThread.start();
-  }
-
-  @Override
-  public void run()
-  {
-    while (!Thread.currentThread().isInterrupted())
-    {
-      try
-      {
-        Thread.sleep(_INTERVAL.toMillis());
-
-        long time = System.currentTimeMillis();
-        KafkaMetricCollector.this.getRoundtripTime();
-        _LOGGER.info("Metric: RoundtripTime: " + (System.currentTimeMillis() - time) + "ms");
-      }
-      catch (Exception e)
-      {
-        _LOGGER.error("Failed to calculate roundtrip time", e);
-      }
-    }
+    // We need this init method to force quarkus to initialize this class.
+    // Can we do this anyhow else?
   }
 
   /**
    * Calculates the current roundtrip time
    */
-  @Timed(name = "roundtripTime", description = "Describes how long a kafka messages takes from a service to kafka and back", unit = MetricUnits.MILLISECONDS)
-  protected void getRoundtripTime()
+  @Gauge(name = "roundtripTime", description = "Describes how long a kafka messages takes from a service to kafka and back", unit = MetricUnits.MILLISECONDS)
+  public long getRoundtripTime()
   {
-    //noinspection ResultOfMethodCallIgnored
-    eventFacade.sendMetricsEvent(new KafkaMetricEvent().started(Instant.now())).blockingGet();
+    KafkaMetricEvent answerEvent = eventFacade.sendMetricsEvent(new KafkaMetricEvent().started(Instant.now())).blockingGet();
+    long result = System.currentTimeMillis() - answerEvent.started().toEpochMilli();
+    _LOGGER.info("RoundtripTime: " + result + "ms");
+    return result;
   }
 
 }
