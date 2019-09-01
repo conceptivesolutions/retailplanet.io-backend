d729079dddd49d05b0f59f9a7e6ea432725e0f7a
w.glanzer <wernerglanzer@googlemail.com>
2019-09-01 11:54:39 +0200

    Set Unirest client on startup

--- sources/base/src/main/java/io/retailplanet/backend/clients/base/impl/Starter.java
+++ sources/base/src/main/java/io/retailplanet/backend/clients/base/impl/Starter.java
@@ -1,6 +1,10 @@
 package io.retailplanet.backend.clients.base.impl;
 
+import com.mashape.unirest.http.Unirest;
 import io.quarkus.runtime.StartupEvent;
+import io.retailplanet.backend.clients.base.impl.util.SSLUtility;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClients;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -23,6 +27,11 @@ public class Starter
    */
   public void init(@SuppressWarnings("unused") @Observes StartupEvent pEvent)
   {
+    // Ignore insecure SSL certs
+    Unirest.setHttpClient(HttpClients.custom()
+                              .setSSLSocketFactory(new SSLConnectionSocketFactory(SSLUtility.createTrustAllContext()))
+                              .build());
+
     if (!thread.isRunning())
       thread.start();
   }
