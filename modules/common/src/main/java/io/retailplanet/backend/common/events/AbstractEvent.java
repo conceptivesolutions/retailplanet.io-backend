edbd88b8c2197b27f44747163b125d8cc063f462
w.glanzer <wernerglanzer@googlemail.com>
2019-09-05 22:16:56 +0200

    Removed useless kafka events, they will be replaced by REST calls

--- modules/common/src/main/java/io/retailplanet/backend/common/events/AbstractEvent.java
+++ modules/common/src/main/java/io/retailplanet/backend/common/events/AbstractEvent.java
@@ -1,17 +1,11 @@
 package io.retailplanet.backend.common.events;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.opentracing.*;
-import io.opentracing.propagation.*;
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import io.reactivex.*;
-import io.retailplanet.backend.common.metrics.*;
 import io.retailplanet.backend.common.util.Utility;
-import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.UUID;
 
 /**
  * @author w.glanzer, 11.07.2019
@@ -22,21 +16,8 @@ public abstract class AbstractEvent<S extends AbstractEvent<S>>
   /* Specifies how long an event lives in milliseconds */
   public static final long TTL = Utility.isDevMode() ? Duration.ofHours(24).toMillis() : Duration.ofSeconds(3).toMillis();
 
-  private static final boolean _TRACE_ENABLED = !Utility.isNullOrEmptyTrimmedString(System.getenv("OPENTRACING_SERVERS"));
-  private static final String _REFERENCE_TYPE_KEY = "__REF_TYPE";
-
   @JsonProperty
   public String chainID = UUID.randomUUID().toString();
-
-  /* Current execution context for OpenTracing propagation */
-  @JsonProperty
-  public Map<String, String> traceContext;
-
-  /* Topic, where this event comes from */
-  String receivedTopic;
-
-  private final IMetricEventFacade metricEventFacade = MetricEventFacade.getInstance();
-  private Span computingSpan;
 
   public <T extends AbstractEvent> T createAnswer(Class<T> pClazz)
   {
@@ -50,83 +31,6 @@ public abstract class AbstractEvent<S extends AbstractEvent<S>>
     {
       throw new RuntimeException(e);
     }
-  }
-
-  /**
-   * Starts to trace the current event computation
-   *
-   * @param pTracer Tracer
-   */
-  void startTrace(@NotNull Tracer pTracer)
-  {
-    if (!_TRACE_ENABLED)
-      return;
-
-    Tracer.SpanBuilder builder = pTracer.buildSpan(getClass().getSimpleName());
-    if (traceContext != null)
-    {
-      String refType = traceContext.get(_REFERENCE_TYPE_KEY);
-      if (refType != null)
-        builder = builder.addReference(refType, pTracer.extract(Format.Builtin.HTTP_HEADERS, new TextMapExtractAdapter(traceContext)));
-    }
-
-    if (receivedTopic != null)
-      builder = builder.withTag("topic", receivedTopic);
-
-    computingSpan = builder.startActive(true).span();
-  }
-
-  /**
-   * Sets the current active span as trace to this event
-   *
-   * @param pTracer Tracer to get the span from
-   */
-  void injectCurrentTrace(@NotNull Tracer pTracer)
-  {
-    if (!_TRACE_ENABLED)
-      return;
-
-    traceContext = new HashMap<>();
-
-    Span span = pTracer.activeSpan();
-    if (span != null)
-    {
-      traceContext.put(_REFERENCE_TYPE_KEY, References.CHILD_OF);
-      pTracer.inject(span.context(), Format.Builtin.HTTP_HEADERS, new TextMapInjectAdapter(traceContext));
-    }
-  }
-
-  /**
-   * Finishes the computation of this event
-   */
-  void finishTrace()
-  {
-    if (!_TRACE_ENABLED)
-      return;
-
-    if (computingSpan != null)
-      computingSpan.finish();
-  }
-
-  /**
-   * Creates a new flowable which waits for a result (and error) with the given chainID on the given flowables
-   *
-   * @return Flowable with filtered chainID and set timeout
-   */
-  @SafeVarargs
-  @NotNull
-  final <T extends AbstractEvent> Single<T> waitForAnswer(@NotNull Flowable<ErrorEvent> pErrors, @NotNull Flowable<? extends T>... pFlowables)
-  {
-    return Flowable.merge(Flowable.merge(Arrays.asList(pFlowables)), pErrors)
-        .timeout(TTL, TimeUnit.MILLISECONDS)
-        .filter(pEvent -> pEvent.chainID.trim().equals(chainID))
-        .map(pEvent -> {
-          if (pEvent instanceof ErrorEvent)
-            throw ((ErrorEvent) pEvent).error();
-          return (T) pEvent;
-        })
-        .firstOrError()
-        .doOnError(metricEventFacade::fireAnswerReceiveError);
   }
 
 }
