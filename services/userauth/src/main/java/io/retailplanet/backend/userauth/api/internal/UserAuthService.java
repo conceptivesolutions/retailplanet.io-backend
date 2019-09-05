0380395573c951aac5955c1f5109a65133d3bf1e
w.glanzer <wernerglanzer@googlemail.com>
2019-09-05 22:44:27 +0200

    Refactored "userauth" to use REST-Client instead of Kafka

---
+++ services/userauth/src/main/java/io/retailplanet/backend/userauth/api/internal/UserAuthService.java
@@ -0,0 +1,26 @@
+package io.retailplanet.backend.userauth.api.internal;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author w.glanzer, 05.09.2019
+ */
+@Path("/internal/userauth")
+public class UserAuthService
+{
+
+  /**
+   * Authenticates a clientID and returns 200, if the client is allowed to use the features within the given scope
+   *
+   * @param pClientID clientID
+   * @param pScope    Scope
+   * @return Response, if allowed or not
+   */
+  @GET
+  public Response validate(@PathParam("clientID") String pClientID, @PathParam("scope") String pScope)
+  {
+    return Response.ok().build(); //todo
+  }
+
+}
